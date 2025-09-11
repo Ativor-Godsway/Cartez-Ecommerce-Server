22f5b8c39b1963aed28b6c63dfd0b4cda2cebf9d
Ativor <ativorgodswaycoco@gmail.com>
2025-09-10 23:43:40 -0700

    Fix CORS config for frontend

--- index.js
+++ index.js
@@ -12,10 +12,17 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://cartez-ecommerce-frontend.vercel.app",
-    ], // Your React frontend URL
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://cartez-ecommerce-frontend.vercel.app",
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
